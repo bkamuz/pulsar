d98aaf5ccd367b69d380d183cf4d2597b913e80f
bkamuz <bkamuz@outlook.com>
2025-11-18 17:37:07 +0300

    Исправил загрузку семплов в colider

---
+++ commands.ps1
@@ -0,0 +1,6 @@
+nvim ./default.js
+
+cd ../strudel
+pnpm run osc  
+
+sclang .\_collider.scd
--- default.js
+++ default.js
@@ -1,6 +1,6 @@
-samples('http://localhost:5432/');
+samples('https://raw.githubusercontent.com/bkamuz/pulsar/refs/heads/main/samples/strudel.json');
 
-$: s("bd*4").scope()
+$: s("Breaks:1*4").fit().scope()
 
 $: n("<0 4 0 9 7>*16").scale("g:minor").trans(-12)
     .s("sawtooth")
